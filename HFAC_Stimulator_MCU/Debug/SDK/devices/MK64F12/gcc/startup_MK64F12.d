1c74490bbf4e33b281c3f1e6c4d15b509701d8ec
adyrapeaux <adrien.rapeaux13@imperial.ac.uk>
2020-04-24 16:10:53 +0100

    Cleaning eclipse project

--- HFAC_Stimulator_MCU/Debug/SDK/devices/MK64F12/gcc/startup_MK64F12.d
+++
@@ -1,2 +0,0 @@
-SDK/devices/MK64F12/gcc/startup_MK64F12.o: \
- C:/Users/adrien/MCU\ Programming/K64F_SDK_NoDebugConsole/devices/MK64F12/gcc/startup_MK64F12.S
--- HFAC_Stimulator_MCU/Debug/Sources/peripherals.d
+++
@@ -1,3 +0,0 @@
-Sources/peripherals.o: ../Sources/peripherals.c ../Sources/peripherals.h
-
-../Sources/peripherals.h:
